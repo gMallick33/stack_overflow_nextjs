6a0ed448b81a8e36181a081b1695176ae802b490
gyani <g.arya.mallick@gmail.com>
2023-10-28 23:59:34 +0530

    added assets in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,45 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
 
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "Next.js 13 with clerk",
+  description:
+    "A community driven platform for discussing technologies, questioning, answering, sharing knowledge and collaborating with developers from around the world.",
+  icons: {
+    icon: "/public/assets/images/site-logo.svg",
+  },
+};
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
+        <h1 className="h1-bold">Hello next.js</h1>
         <body>{children}</body>
       </html>
     </ClerkProvider>
