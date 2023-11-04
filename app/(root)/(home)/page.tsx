942d8763e3c2d6cd0e949a5a634be4c4e1afdfd0
gyani <g.arya.mallick@gmail.com>
2023-11-05 02:41:25 +0530

    added ui for "All Questions" and "Ask a Question"

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,21 @@
+import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>{/* <UserButton afterSignOutUrl="/" /> */}</div>;
+  return (
+    <div>
+      <div
+        className="flex w-full flex-col-reverse 
+    justify-between gap-4 sm:flex-row sm:items-center"
+      >
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
