ef6486a48280c402c88392b2aa01da7e0bdcc19a
gyani <g.arya.mallick@gmail.com>
2023-11-04 20:15:06 +0530

    implemented LeftSideBar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import LeftSideBar from "@/components/shared/LeftSideBar";
 import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 
@@ -11,7 +12,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 
       <div className="flex">
         {/** LeftSideBar */}
-        LeftSideBar
+        <LeftSideBar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
---
+++ components/shared/LeftSideBar.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import { sidebarLinks } from "@/constants/constants";
+import React from "react";
+import { SheetClose } from "../ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { SignedOut } from "@clerk/nextjs";
+
+const LeftSideBar = () => {
+  const pathname = usePathname();
+  return (
+    <section
+      className="background-light900_dark200 light-border
+    custom-scrollbar sticky left-0 top-0 flex h-screen
+    flex-col overflow-y-auto border-r pt-28 shadow-light-300
+    dark:shadow-none max-sm:hidden lg:w-[266px]"
+    >
+      <div className="flex flex-1 flex-col gap-1">
+        {sidebarLinks.map((item) => {
+          const isActive =
+            (pathname.includes(item.route) && item.route.length > 1) ||
+            pathname === item.route;
+
+          return (
+            <Link
+              key={item.route}
+              href={item.route}
+              className={`${
+                isActive
+                  ? "primary-gradient rounded-lg text-light-900"
+                  : "text-dark300_light900"
+              } flex items-center justify-start gap-4
+        bg-transparent p-4`}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={20}
+                height={20}
+                className={`${isActive ? "" : "invert-colors"}`}
+              />
+              <p
+                className={`${
+                  isActive ? "base-bold" : "base-medium"
+                } max-lg:hidden`}
+              >
+                {item.label}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+
+      <SignedOut>
+        <div className="flex flex-col gap-3">
+          <Link href="/sign-in">
+            <Button
+              className="small-medium btn-secondary
+                  min-h-[41px] w-full rounded-lg px-4 py-3"
+            >
+              <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/c/ce/Google_account_icon.svg"
+                alt="signin"
+                width={20}
+                height={20}
+                className="invert-colors lg:hidden"
+              />
+              <span className="primary-text-gradient max-lg:hidden">
+                Sign In
+              </span>
+            </Button>
+          </Link>
+
+          <Link href="/sign-up">
+            <Button
+              className="small-medium btn-tertiary
+                  light-border-2 text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none"
+            >
+              <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/9/9f/New_user_icon-01.svg"
+                alt="signup"
+                width={20}
+                height={20}
+                className="invert-colors lg:hidden"
+              />
+              <span className="max-lg:hidden">Sign Up</span>
+            </Button>
+          </Link>
+        </div>
+      </SignedOut>
+    </section>
+  );
+};
+
+export default LeftSideBar;
