3b5606b4a387b79ade00aa240d488f158a6b2ced
gyani <g.arya.mallick@gmail.com>
2023-11-07 16:45:22 +0530

    implemented "user" and "question" model

---
+++ database/user.model.ts
@@ -0,0 +1,35 @@
+import { Schema, models, model, Document } from "mongoose";
+
+export interface IUser extends Document {
+  clerkId: string;
+  name: string;
+  username: string;
+  email: string;
+  password?: string;
+  bio?: string;
+  picture: string;
+  location: string;
+  portfolioWebsite?: string;
+  reputation?: number;
+  saved: Schema.Types.ObjectId[];
+  joinedAt: Date;
+}
+
+const UserSchema = new Schema({
+  clerkId: { type: String, required: true },
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String }, // You can add validation and hashing for passwords
+  bio: { type: String },
+  picture: { type: String, required: true },
+  location: { type: String },
+  portfolioWebsite: { type: String },
+  reputation: { type: Number, default: 0 },
+  saved: [{ type: Schema.Types.ObjectId, ref: "Question" }], // Reference to the Question model
+  joinedAt: { type: Date, default: Date.now },
+});
+
+const User = models.Question || model("User", UserSchema);
+
+export default User;
