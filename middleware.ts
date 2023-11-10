daf95f185aecb8f38d0d1ce6823d77b6dd580575
gyani <g.arya.mallick@gmail.com>
2023-11-10 18:03:26 +0530

    added "/profile",     "/jobs",     "/community",     "/tags",     "/collection", to publicRoutes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,15 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/webhooks(.*)", "/"],
+  publicRoutes: [
+    "/api/webhooks(.*)",
+    "/",
+    "/profile",
+    "/jobs",
+    "/community",
+    "/tags",
+    "/collection",
+  ],
 });
 
 export const config = {
