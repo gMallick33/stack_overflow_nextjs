6d4e613df0aceae1a5f65f56468f8d89a4b193be
gyani <g.arya.mallick@gmail.com>
2023-11-13 14:48:02 +0530

    minor fixes

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
-          src="/assets/images/site-logo.svg"
+          src="/assets/icons/site-logo.svg"
           width={23}
           height={23}
           alt="DevoverFlow"
--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-    mdxRs: true,
-    serverComponentsExternalPackages: ["mongoose"],
-  },
   images: {
     remotePatterns: [
       {
