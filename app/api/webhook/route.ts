5d876e78d99f12a72be0d262a8968bf81e942ea7
gyani <g.arya.mallick@gmail.com>
2023-11-13 07:15:10 +0530

    added a blank line

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -74,6 +74,7 @@ export async function POST(req: Request) {
 
   if (eventType === "user.updated") {
     console.log("event: user updated");
+
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
 
