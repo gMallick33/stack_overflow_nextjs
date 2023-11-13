16c0a54f69aa593c48bdee6c9d2d449da2379f42
gyani <g.arya.mallick@gmail.com>
2023-11-13 14:59:31 +0530

    changed db name

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -15,7 +15,7 @@ export const connectToDatabase = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: "devflow",
+      dbName: "devoverflow",
     });
 
     isConnected = true;
