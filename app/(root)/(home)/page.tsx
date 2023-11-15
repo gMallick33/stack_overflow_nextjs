3828b89e13be53b4b5a618117a20e14a55073af0
gyani <g.arya.mallick@gmail.com>
2023-11-16 00:19:46 +0530

    added the typescript code in {}, inside div

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -98,32 +98,32 @@ export default async function Home() {
       </div>
       <HomeFilters />
 
-      <div className="mt-10 flex w-full flex-col gap-6"></div>
-
-      {result.questions.length > 0 ? (
-        result.questions.map((question) => (
-          <QuestionCard
-            key={question._id}
-            _id={question._id}
-            title={question.title}
-            tags={question.tags}
-            author={question.author}
-            upvotes={question.upvotes}
-            views={question.views}
-            answers={question.answers}
-            createdAt={question.createdAt}
-          />
-        ))
-      ) : (
-        <NoResult
-          title="There is no question to show"
-          description="Be the first to break the silence? Ask a Question and kickstart the
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There is no question to show"
+            description="Be the first to break the silence? Ask a Question and kickstart the
         discussion. our query could be the next big thing others learn from. Get
         involved!"
-          link="/ask-question"
-          linkTitle="Ask a Question"
-        />
-      )}
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </div>
   );
 }
