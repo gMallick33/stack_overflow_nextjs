6233ea5d8d80d169aa94e2aad0f17723942cac6a
gyani <g.arya.mallick@gmail.com>
2023-11-16 01:42:04 +0530

    updated middleware.ts publicRoutes and ignoredRoutes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,18 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/webhooks(.*)"],
+  publicRoutes: [
+    "/",
+    "/api/webhook",
+    "/question/:id",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
+
+  ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
 });
 
 export const config = {
