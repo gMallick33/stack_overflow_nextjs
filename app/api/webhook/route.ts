8ba03779d6d3c6a05d4a4e487d5c0a386b72657d
gyani <g.arya.mallick@gmail.com>
2023-11-16 02:42:33 +0530

    updated to console.log({eventType})

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -55,7 +55,7 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
-  console.log(eventType);
+  console.log({ eventType });
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
