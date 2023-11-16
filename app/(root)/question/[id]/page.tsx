cf331e61badf33d51f7b592556f442ef95f38660
gyani <g.arya.mallick@gmail.com>
2023-11-17 01:33:13 +0530

    implemented title, question asked, views, answers in question/[id]/

---
+++ app/(root)/question/[id]/page.tsx
@@ -0,0 +1,67 @@
+import Metric from "@/components/shared/Metric";
+import ParseHTML from "@/components/shared/ParseHTML";
+import { getQuestionById } from "@/lib/actions/question.action";
+import { formatBigNumber, getTimeStamp } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Page = async ({ params, searchParams }) => {
+  const result = await getQuestionById({ questionId: params.id });
+  return (
+    <>
+      <div className="flex-start w-full flex-col">
+        <div className="flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+          <Link
+            href={`/profile/${result.author.clerkId}`}
+            className="flex items-center justify-start gap-1"
+          >
+            <Image
+              src={result.author.picture}
+              className="rounded-full"
+              width={22}
+              height={22}
+              alt="profile"
+            />
+            <p className="paragraph-semibold text-dark300_light700">
+              {result.author.name}
+            </p>
+          </Link>
+
+          <div className="flex justify-end">VOTING</div>
+        </div>
+        <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
+          {result.title}
+        </h2>
+        <div className="mb-8 mt-5 flex flex-wrap gap-4">
+          <Metric
+            imgUrl="/assets/icons/clock.svg"
+            value={` asked ${getTimeStamp(result.createdAt)}`}
+            alt="asked"
+            title=""
+            textStyles="small-medium text-dark400_light800"
+          />
+
+          <Metric
+            imgUrl="/assets/icons/message.svg"
+            value={formatBigNumber(result.answers.length)}
+            alt="message"
+            title=" Answers"
+            textStyles="small-medium text-dark400_light800"
+          />
+
+          <Metric
+            imgUrl="/assets/icons/eye.svg"
+            value={formatBigNumber(result.views)}
+            alt="eye"
+            title=" Views"
+            textStyles="small-medium text-dark400_light800"
+          />
+        </div>
+      </div>
+      <ParseHTML data={result.content} />
+    </>
+  );
+};
+
+export default Page;
