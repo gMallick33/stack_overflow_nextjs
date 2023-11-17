e80ab1c56248da10923d63549598ba5745293019
gyani <g.arya.mallick@gmail.com>
2023-11-18 00:03:09 +0530

    implemented to show answers of questions

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -1,7 +1,9 @@
 import Answer from "@/components/forms/Answer";
+import AllAnswers from "@/components/shared/AllAnswers";
 import Metric from "@/components/shared/Metric";
 import ParseHTML from "@/components/shared/ParseHTML";
 import RenderTag from "@/components/shared/RenderTag";
+import Votes from "@/components/shared/Votes";
 import { getQuestionById } from "@/lib/actions/question.action";
 import { getUserById } from "@/lib/actions/user.action";
 import { formatBigNumber, getTimeStamp } from "@/lib/utils";
@@ -37,7 +39,9 @@ const Page = async ({ params, searchParams }) => {
             </p>
           </Link>
 
-          <div className="flex justify-end">VOTING</div>
+          <div className="flex justify-end">
+            <Votes />
+          </div>
         </div>
         <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
           {result.title}
@@ -79,6 +83,11 @@ const Page = async ({ params, searchParams }) => {
           />
         ))}
       </div>
+      <AllAnswers
+        questionId={result._id}
+        userId={JSON.stringify(mongoUser._id)}
+        totalAnswers={result.answers.length}
+      />
       <Answer
         question={result.content}
         questionId={JSON.stringify(result._id)}
---
+++ components/shared/AllAnswers.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import Filter from "./Filter";
+import { AnswerFilters } from "@/constants/filters";
+import { getAnswers } from "@/lib/actions/answer.action";
+import Link from "next/link";
+import Image from "next/image";
+import { getTimeStamp } from "@/lib/utils";
+import ParseHTML from "./ParseHTML";
+
+interface Props {
+  questionId: string;
+  userId: string;
+  totalAnswers: number;
+  page?: number;
+  filter?: number;
+}
+
+const AllAnswers = async ({
+  questionId,
+  userId,
+  totalAnswers,
+  page,
+  filter,
+}: Props) => {
+  const result = await getAnswers({
+    questionId,
+  });
+
+  return (
+    <div className="mt-11">
+      <div className="flex items-center justify-between">
+        <h3 className="primary-text-gradient">{totalAnswers} Answers</h3>
+        <Filter
+          filters={AnswerFilters}
+          otherClasses={""}
+          containerClasses={""}
+        />
+      </div>
+      <div>
+        {result.answers.map((answer) => (
+          <article key={answer._id} className="light-border border-b py-10">
+            <div className="flex items-center justify-between">
+              {/** SPAN ID */}
+              <div className="mb-8 flex flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+                <Link
+                  href={`/profile/${answer.author.clerkId}`}
+                  className="flex flex-1 items-start gap-1 sm:items-center"
+                >
+                  <Image
+                    src={answer.author.picture}
+                    width={18}
+                    height={18}
+                    alt="profile"
+                    className="rounded-full object-cover max-sm:mt-0.5"
+                  />
+                  <div className="flex flex-col sm:flex-row sm:items-center">
+                    <p className="body-semibold text-dark300_light700">
+                      {answer.author.name}
+                    </p>
+                    <p className="small-regular text-light400_light500 ml-1 mt-0.5 line-clamp-1">
+                      answered {getTimeStamp(answer.createdAt)}
+                    </p>
+                  </div>
+                </Link>
+                <div className="flex justify-end">VOTING</div>
+              </div>
+            </div>
+            <ParseHTML data={answer.content} />
+          </article>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AllAnswers;
