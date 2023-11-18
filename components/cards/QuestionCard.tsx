01efaa8477b557d5c0ee444dc6ad6bd7e4718f81
gyani <g.arya.mallick@gmail.com>
2023-11-18 23:18:38 +0530

    implemented "collection" page to for saved question

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -16,7 +16,7 @@ interface QuestionProps {
     name: string;
     picture: string;
   };
-  upvotes: number;
+  upvotes: string[];
   views: number;
   answers: Array<object>;
   createdAt: Date;
@@ -71,7 +71,7 @@ const QuestionCard = ({
 
         <Metric
           imgUrl="/assets/icons/like.svg"
-          value={formatBigNumber(upvotes)}
+          value={formatBigNumber(upvotes.length)}
           alt="upvotes"
           title=" Votes"
           textStyles="small-medium text-dark400_light800"
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -6,12 +6,15 @@ import {
   CreateUserParams,
   DeleteUserParams,
   GetAllUsersParams,
+  GetSavedQuestionsParams,
   GetUserByIdParams,
   ToggleSaveQuestionParams,
   UpdateUserParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
 import Question from "@/database/question.model";
+import Tag from "@/database/tag.model";
+import { FilterQuery } from "mongoose";
 
 export async function getUserById(params: GetUserByIdParams) {
   try {
@@ -124,6 +127,38 @@ export async function toggleSaveQuestion(params: ToggleSaveQuestionParams) {
       );
     }
     revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getSavedQuestion(params: GetSavedQuestionsParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
+
+    const query: FilterQuery<typeof Question> = searchQuery
+      ? { title: { $regex: new RegExp(searchQuery, "i") } }
+      : {};
+    const user = await User.findOne({ clerkId }).populate({
+      path: "saved",
+      match: query,
+      options: {
+        sort: { createdAt: -1 },
+      },
+      populate: [
+        { path: "tags", model: Tag, select: "_id name" },
+        { path: "author", model: User, select: "_id clerkId name picture" },
+      ],
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+    const savedQuestion = user.saved;
+    return { questions: savedQuestion };
   } catch (error) {
     console.log(error);
     throw error;
