a9459578eb795e2df2e030ba6f8cda39a959240d
gyani <g.arya.mallick@gmail.com>
2023-11-24 05:20:48 +0530

    implemented filters for tags page

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -10,11 +10,12 @@ import React from "react";
 const Page = async ({ searchParams }: SearchParamsProps) => {
   const result = await getAllTags({
     searchQuery: searchParams.q,
+    filter: searchParams.filter,
   });
 
   return (
     <>
-      <h1 className="h1-bold text-dark100_light900">All Users</h1>
+      <h1 className="h1-bold text-dark100_light900">All Tags</h1>
 
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearchBar
