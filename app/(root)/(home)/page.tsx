bb1fb0f3b0a059ea1eda0d705b65ae1de1b9b1a3
gyani <g.arya.mallick@gmail.com>
2023-11-24 04:14:10 +0530

    implemented newest, frequent, unanswered filters in the Home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -13,9 +13,10 @@ import Link from "next/link";
 export default async function Home({ searchParams }: SearchParamsProps) {
   const result = await getQuestions({
     searchQuery: searchParams.q,
+    filter: searchParams.filter,
   });
-  // console.log(result.questions);
 
+  // fetch Recommended Question
   return (
     <div>
       <div
