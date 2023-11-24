f88aba02e03b27505d15670395a9fc53506c3b19
gyani <g.arya.mallick@gmail.com>
2023-11-24 05:33:51 +0530

    implemented filters for answers in question/[id] page

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -96,6 +96,8 @@ const Page = async ({ params, searchParams }) => {
         questionId={result._id}
         userId={mongoUser._id}
         totalAnswers={result.answers.length}
+        page={searchParams?.page}
+        filter={searchParams?.filter}
       />
       <Answer
         question={result.content}
