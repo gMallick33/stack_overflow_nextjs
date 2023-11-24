310fda3bdc91cf3ba22b8fb499356ab97f61dd87
gyani <g.arya.mallick@gmail.com>
2023-11-24 07:08:38 +0530

    implemented pagination for home page

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -21,7 +21,11 @@ export async function getQuestions(params: GetQuestionsParams) {
   try {
     connectToDatabase();
 
-    const { searchQuery, filter } = params;
+    const { searchQuery, page = 1, pageSize = 2, filter } = params;
+
+    // calculate the number of posts to skip based on the page number and page size
+    const skipAmount = (page - 1) * pageSize;
+
     const query: FilterQuery<typeof Question> = {};
     if (searchQuery) {
       query.$or = [
@@ -49,9 +53,14 @@ export async function getQuestions(params: GetQuestionsParams) {
     const questions = await Question.find(query)
       .populate({ path: "tags", model: Tag })
       .populate({ path: "author", model: User })
+      .skip(skipAmount)
+      .limit(pageSize)
       .sort(sortOptions);
 
-    return { questions };
+    const totalQuestions = await Question.countDocuments(query);
+    const isNext = totalQuestions > skipAmount + questions.length;
+
+    return { questions, isNext };
   } catch (error) {
     console.log(error);
     throw error;
