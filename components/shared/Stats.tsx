aa98f9cf5c4dd6b2bd34c2ddf32f8661d6290be8
gyani <g.arya.mallick@gmail.com>
2023-11-25 18:48:28 +0530

    implemented Badge system, reputation, gold, silver, bronze medals

--- components/shared/Stats.tsx
+++ components/shared/Stats.tsx
@@ -1,4 +1,5 @@
 import { formatBigNumber } from "@/lib/utils";
+import { BadgeCounts } from "@/types";
 import Image from "next/image";
 import React from "react";
 
@@ -11,6 +12,8 @@ interface StatsCardProps {
 interface Props {
   totalQuestions: number;
   totalAnswers: number;
+  badges: BadgeCounts;
+  reputation: number;
 }
 
 const StatsCard = ({ imgUrl, value, title }: StatsCardProps) => {
@@ -25,10 +28,12 @@ const StatsCard = ({ imgUrl, value, title }: StatsCardProps) => {
   );
 };
 
-const Stats = ({ totalQuestions, totalAnswers }: Props) => {
+const Stats = ({ reputation, totalQuestions, totalAnswers, badges }: Props) => {
   return (
     <div className="mt-10">
-      <h4 className="h3-semibold text-dark200_light900">Stats</h4>
+      <h4 className="h3-semibold text-dark200_light900">
+        Stats - {reputation}
+      </h4>
       <div className="mt-5 grid grid-cols-1 gap-5 xs:grid-cols-2 md:grid-cols-4">
         <div className="light-border background-light900_dark300 flex flex-wrap items-center justify-evenly gap-4 rounded-md border p-6 shadow-light-300 dark:shadow-dark-200">
           <div>
@@ -46,17 +51,17 @@ const Stats = ({ totalQuestions, totalAnswers }: Props) => {
         </div>
         <StatsCard
           imgUrl="/assets/icons/gold-medal.svg"
-          value={0}
+          value={badges.GOLD}
           title="Gold Badges"
         />
         <StatsCard
           imgUrl="/assets/icons/silver-medal.svg"
-          value={0}
+          value={badges.SILVER}
           title="Silver Badges"
         />
         <StatsCard
           imgUrl="/assets/icons/bronze-medal.svg"
-          value={0}
+          value={badges.BRONZE}
           title="Bronze Badges"
         />
       </div>
