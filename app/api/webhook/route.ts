743cf3afbbc32cbfc29946bb78a0d6e4ab01dbd0
gyani <g.arya.mallick@gmail.com>
2023-11-26 13:45:35 +0530

    minor fix

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -106,5 +106,5 @@ export async function POST(req: Request) {
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
   console.log("Webhook body:", body);
 
-  return new Response("", { status: 200 });
+  return NextResponse.json({ message: "OK" });
 }
