152e82b3e1227d5d93ddd3404bfbb9482588eafc
gyani <g.arya.mallick@gmail.com>
2023-11-27 18:46:55 +0530

    temp work in progress for jobs page

---
+++ app/(root)/jobs/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Job? Whats that</h1>
+      <h3 className="h3-semibold text-dark100_light900 mt-10">
+        It is work in progress 🙂
+      </h3>
+    </div>
+  );
+};
+
+export default page;
